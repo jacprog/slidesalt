ea61f073b42af223325371208f462462de6ee5b7
Jacques Favre <jacprog@users.noreply.github.com>
2018-05-23 23:46:40 -0400

    Add files via upload

---
+++ js/paraslider.js
@@ -0,0 +1,10 @@
+var firstparallaxslider=new parallaxSlider({
+wrapperid: 'myparallaxslider', //ID of DIV on page to house slider
+displaymode: {type:'manual', pause:1000, cycles:0, stoponclick:true, pauseonmouseover:true},
+delaybtwdesc: 500, // delay in milliseconds between the revealing of each description layer inside a slide
+navbuttons: ['../icon/left.png', '../icon/right.png'], // path to nav images
+activeslideclass: 'selectedslide', // CSS class that gets added to currently shown DIV slide
+orientation: 'h', //Valid values: "h" or "v"
+persist: true, //remember last viewed slide and recall within same session?
+slideduration: 1000 //transition duration (milliseconds)
+});
